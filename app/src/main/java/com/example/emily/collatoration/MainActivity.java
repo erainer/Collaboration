c0aa77de0ec8a355e7ebb1bc866d2c5864cfacf9
erainer <31553480+erainer@users.noreply.github.com>
2018-09-01 15:55:38 -0500

    Connecting the second screen to the home screen.

--- app/src/main/java/com/example/emily/collatoration/MainActivity.java
+++ app/src/main/java/com/example/emily/collatoration/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.emily.collatoration;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Log.d("Adam","was here");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Adam","was here");
 
         Button btn1 = (Button) findViewById(R.id.btn1);
 
